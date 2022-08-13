00015ee2b89067e6c64667cf890a2a60e8f9b012
daniilakulov <sladkoeshka33@yandex.ru>
2022-08-13 13:06:40 +0700

    Решил задачи

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+        //Задание 1
+        int number = 0;
+        while( number < 10 ) {
+            System.out.print(++number + " ");
+        }
+        System.out.println();
+        for (; number > 0; --number) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
+
+        //Задача 2
+        int Friday = 5;
+        for ( int nextFriday = Friday; nextFriday <= 31; nextFriday += 7 ){
+            System.out.println("Сегодня пятница, " + nextFriday + " число. Необходимо подготовить отчет.");
+        }
+
+        //Задача 3
+        int nextYear = 2022;
+        int start = nextYear - 200;
+        int end = nextYear + 100;
+        for (int year = start; year < end; year ++){
+            if ( year % 79 == 0){
+                System.out.println(year);
+            }
+        }
+
+
+
 
 
     }
